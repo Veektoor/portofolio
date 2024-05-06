de364b99cc8a28f0273173b7eba077ab710cab58
Veektoor <gichuhivictorm@gmail.com>
2024-05-06 14:51:34 +0300

    changes

--- account/urls.py
+++ account/urls.py
@@ -1,18 +1,20 @@
 from django.contrib import admin
 from django.urls import path
-from .views import index
-from . import views
+from aacount import views
+
+
+
+# django admin changes
+admin.site.site_header = "Login to site"
+admin.site.site_title = "Welcome to DashBord"
+admin.site.index_title = "Welcome to Portal"
+
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('index', index, name='index'),
-    path('', views.index, name='index'),
-    path('contact/', views.contact, name='contact'),
-    path('projects/', views.projects, name='projects'),
-    path('skills/', views.skills, name='skills'),
-    path('blog/', views.blog, name='blog'),
-    path('blog/<slug:slug>/', views.blog_detail, name='blog_detail'),
-    path('contact_success/', views.contact_success, name='contact_success'),
-    path('about/', views.about, name='about'),
+    path('', views.home, name='home'),
+    path('project', views.project, name='project'),
+    path('contact', views.contact, name='contact'),
+    # path('skills', views.skills, name='skills'),
+    # path('contact', views.contact, name='contact'),
 ]
--- account/views.py
+++ account/views.py
@@ -1,63 +1,29 @@
-from django.shortcuts import render, redirect
-from .models import Project, Skill, Contact, Blog
-from .forms import ContactForm
+from django.shortcuts import render, HttpResponse
 
-def index(request):
-    projects = Project.objects.all()
-    skills = Skill.objects.all()
-    blogs = Blog.objects.all()
-    context = {
-        'projects': projects,
-        'skills': skills,
-        'blogs': blogs,
-    }
-    return render(request, 'index.html', context)
+def home(request):
+    #contact form database
+    # if request.method == 'POST':
+    #     name == request.POST['name']
+    #     email == request.POST['email']
+    #     subject == request.POST['subject']
+    #     message == request.POST['message']
+    #     contact = models.Home(name=name, email=email, subject=subject, message=message)
+    #     contact.save()
+    return render(request, 'home.html')
+
+
+def project(request):
+    return render(request, 'project.html')
+
 
 def contact(request):
-    if request.method == 'POST':
-        form = ContactForm(request.POST)
-        if form.is_valid():
-            name = form.cleaned_data['name']
-            email = form.cleaned_data['email']
-            message = form.cleaned_data['message']
-            Contact.objects.create(name=name, email=email, message=message)
-            # You can add a success message or redirect here
-            return redirect('contact_success')  # Redirect to a success page
-    else:
-        form = ContactForm()
-    return render(request, 'contact.html', {'form': form})
+    #contact form database
+    if request.method == "POST":
+        name == request.POST['name']
+        email == request.POST['email']
+        subject == request.POST['subject']
+        message == request.POST['message']
+        contact = models.Home(name=name, email=email, subject=subject, message=message)
+        contact.save()
+    return render(request, 'home.html')
 
-def contact_success(request):
-    return render(request, 'contact_success.html')
-
-
-def blog(request):
-    blogs = Blog.objects.filter(status=1).order_by('-published_date')
-    context = {
-        'blogs': blogs,
-    }
-    return render(request, 'blog.html', context)
-
-def blog_detail(request, slug):
-    blog = Blog.objects.get(slug=slug)
-    context = {
-        'blog': blog,
-    }
-    return render(request, 'blog_detail.html', context)
-
-def projects(request):
-    projects = Project.objects.all()
-    context = {
-        'projects': projects,
-    }
-    return render(request, 'projects.html', context)
-
-def skills(request):
-    skills = Skill.objects.all()
-    context = {
-        'skills': skills,
-    }
-    return render(request, 'skills.html', context)
-
-def about(request):
-    return render(request, 'about.html')
